eae1a29dbb672f1bb3f1d650658977752a7006c9
feapolina <fe.cavalcanti2016@gmail.com>
2023-07-14 19:42:08 -0300

    primeiro commit

---
+++ f0den3t.py
@@ -0,0 +1,22 @@
+import pyfiglet
+
+print('')
+text = pyfiglet.figlet_format(text="FODE N E T",
+font="cosmic")
+
+print(text)
+print('')
+print('          ==============================================================')
+print('          |                Feito por: Felipe Cavalcanti                |')
+print('          |                          14/07/2023                        |')
+print('          ==============================================================')
+print('')
+print('            [ 0 ] Instalar todas ferramentas.                           ')
+print('            [ 1 ] Mostrar todas ferramentas.                            ')
+print('            [ 2 ] Categoria de ferramentas.                             ')
+print('            [ 3 ] Atualizar script.                                     ')
+print('            [ 4 ] Sobre o script.                                       ')
+print('            [ X ] Sair.                                                 ')
+print('')
+print('          ______________________________________________________________')
+print('          ==============================================================')
